fb0f3fc6d7996a53a24e8bfcea2be8ed086a627e
Fergal Riordan <riordanf@tcd.ie>
2023-01-27 09:20:56 +0000

    Added HelloWorld project

---
+++ HelloWorld/src/HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+package HelloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+		for(int i = 0; i<10; i++) {
+			System.out.println("Hello world!" + i + "\n");
+		}
+		
+	}
+
+}
